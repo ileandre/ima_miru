c53e6cc6e6ac1456fb6aefee799896c1ab7bb13a
irma leandre <irmaleandre@irmas-Air.PK5001Z>
2021-05-11 01:12:43 -0400

    changed production url

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -8,7 +8,7 @@ const getToken = () => {
 
 const api = axios.create({
     baseURL: process.env.NODE_ENV === 'production'
-        ? 'https://ima-miru-01.herokuapp.com/api'
+        ? 'https://ima-miru.herokuapp.com/api'
         : 'http://localhost:3000/api'
 })
 
